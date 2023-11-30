aa74a2822dfb3c67f25b358044c4610896ff0de2
marcusmedhage <marcusmedhage@gmail.com>
2023-11-30 12:32:26 +0100

    Added main node with SDR and debug for channel switching.

--- rpi-main-unit/sdr_mesh_main.py
+++ rpi-main-unit/sdr_mesh_main.py
@@ -4,7 +4,8 @@ import argparse
 import struct
 from enum import Enum
 import random
-from pyrf24 import RF24, RF24Network, RF24Mesh, RF24NetworkHeader, RF24_PA_LOW, RF24_2MBPS, MESH_DEFAULT_ADDRESS
+from pyrf24 import RF24, RF24Network, RF24Mesh, RF24NetworkHeader, RF24_2MBPS, MESH_DEFAULT_ADDRESS
+from pyrf24 import RF24_PA_LOW, RF24_PA_MIN, RF24_PA_HIGH
 
 start = time.monotonic()
 
@@ -29,9 +30,10 @@ default_channel = 85
 
 nodes = [5]
 
+
 if not radio.begin():
     raise OSError("Radio hardware not responding")
-radio.set_pa_level(RF24_PA_LOW)
+radio.set_pa_level(RF24_PA_HIGH, False)
 
 if not mesh.begin(default_channel):
     if radio.is_chip_connected:
@@ -50,6 +52,13 @@ TIMER = 0
 
 def checkIncomingData():
 
+    has_payload, pipe_number = radio.available_pipe()
+    has_payload = radio.available()
+    if has_payload:
+        print(pipe_number)
+        msg = radio.read(32)
+        print(repr(msg))
+
     while network.available():
         print("network available")
         header, payload = network.read()
@@ -61,16 +70,25 @@ def checkIncomingData():
             print(f"Package from: {senderID}")
             print(f"C: {C}      H: {H}")
 
+            #radio.print_pretty_details()
 
-def sendChannelSwitch(new_channel, node):
+
+def sendChannelSwitch(new_channel, node, debug = False):
     msg = struct.pack("B", new_channel)
     radio.stopListening()
-    mesh.write(msg, CONFIG_COMMAND, node)
+    ok = mesh.write(msg, CONFIG_COMMAND, node)
     radio.startListening()
+    if not ok and debug:
+        print("Problem sending switch command to node: " + str(node))
+    elif debug:
+        print("Switch command received by node: " + str(node))
+    return ok
 
-def sendChannelSwitchAll(new_channel):
-    for node in nodes:
-        sendChannelSwitch(new_channel, node)
+def sendChannelSwitchAll(new_channel, debug = False):
+    oks = [False]*len(nodes)
+    for i, node in enumerate(nodes):
+        oks[i] = sendChannelSwitch(new_channel, node, debug)
+    return oks
 
 try:
     while True:
