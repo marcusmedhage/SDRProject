b5b159049897e9852f489cb330392d64aa3f53ce
marcusmedhage <marcusmedhage@gmail.com>
2023-12-13 14:08:00 +0100

    Code cleanup.

--- rpi-main-unit/web/display_measurements.py
+++ rpi-main-unit/web/display_measurements.py
@@ -9,32 +9,17 @@ app = Flask(__name__)
 @app.route('/')
 def show_measurements():
     
-    nodes = [e[0] for e in get_nodes()]
-    print(nodes[0])
-    print(type(nodes[0]))
-    raw_measurements = [
-        {'node_id': 1, 'type': 'Temperature', 'value': 22.5, 'timestamp': '2023-03-15 12:00:00'},
-        {'node_id': 1, 'type': 'Humidity', 'value': 45, 'timestamp': '2023-03-15 12:05:00'},
-        {'node_id': 2, 'type': 'Temperature', 'value': 23, 'timestamp': '2023-03-15 12:10:00'},
-    ]
-    measurements_by_node = {}
+    nodes = [e[0] for e in get_nodes()] # extracts all nodes in database
+
+    measurements_by_node = {} 
     for node in nodes:
         data = get_measurement_latest(node)
-        #print(data)
         for p in data:
             if node not in measurements_by_node:
                 measurements_by_node[node] = []
             measurements_by_node[node].append({'node_id': p[0], 'type': p[2], 'value': p[3], 'timestamp': p[1]})
 
-    #for measurement in raw_measurements:
-    #    node_id = measurement['node_id']
-    #    if node_id not in measurements_by_node:
-    #        measurements_by_node[node_id] = []
-    #    measurements_by_node[node_id].append(measurement) # A list of measurements
-
     return render_template('measurements.html', measurements_by_node=measurements_by_node)
-
-
 
 
 if __name__ == '__main__':
